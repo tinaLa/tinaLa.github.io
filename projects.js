563c0d62ab27e3d38be882f7b1f8794ea09b75a4
Tina La <tina.la47@gmail.com>
2020-01-24 18:48:28 -0500

    Adjust style of project filter buttons

--- projects.js
+++ projects.js
@@ -4,6 +4,8 @@ function filterAllProjects() {
     for(i = 0; i < projects.length; i++) {
         projects[i].classList.add("visible");
     }
+    
+    activeButton("all");
 }
 
 function filterTechnicalProjects() {
@@ -16,6 +18,8 @@ function filterTechnicalProjects() {
             projects[i].classList.remove("visible");
         }
     }
+    
+    activeButton("technical");
 }
 
 function filterDesignProjects() {
@@ -28,10 +32,26 @@ function filterDesignProjects() {
             projects[i].classList.remove("visible");
         }
     }
+    
+    activeButton("design");
 }
 
-function activeButton() {
+function activeButton(type) {
     let all = document.getElementById("all-button");
     let technical = document.getElementById("technical-button");
     let design = document.getElementById("design-button");
+    
+    if (type === "all") {
+        all.classList.add("active");
+        technical.classList.remove("active");
+        design.classList.remove("active");
+    } else if (type === "technical") {
+        all.classList.remove("active");
+        technical.classList.add("active");
+        design.classList.remove("active");
+    } else {
+        all.classList.remove("active");
+        technical.classList.remove("active");
+        design.classList.add("active");
+    }
 }
