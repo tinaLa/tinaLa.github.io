3aea9ce0b57d2720065da42e7acee9df06ea3feb
Tina La <tina.la47@gmail.com>
2020-07-09 23:07:03 -0400

    Clean up unused files

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-function showMenu() {
-    let links = document.getElementsByClassName("nav-link");
-    
-    for(i = 0; i < links.length; i++) {
-        links[i].classList.add("responsive");
-    }
-}
